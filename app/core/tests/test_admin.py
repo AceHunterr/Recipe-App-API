aeda2077274d4fc090bdcf50836481fc6bd36f90
AceHunterr <aggarwalmehul26@gmail.com>
2023-07-07 02:51:26 +0530

    Added the complete admin user

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -1,10 +1,9 @@
 """
 Tests for the Django admin modifications.
 """
-from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
 from django.urls import reverse
-from django.test import Client
 
 
 class AdminSiteTests(TestCase):
@@ -31,3 +30,17 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test the create user page works."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
